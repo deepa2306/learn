fe83225be813018b87764ba3ea33f8f4b1c35cdf
Deepa <deepa.selvarajk@gmail.com>
2024-05-12 14:54:42 +0530

    react ex

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
      <p>React is a framework that employs Webpack to automatically compile React, JSX, and ES6 code while handling CSS file prefixes. 
       React is a JavaScript-based UI development library. 
       Although React is a library rather than a language, 
-      it is widely used in web development.</p>
+      it is widely used in web development. jhgyg</p>
       </Welcome>
      <Welcome name="krishna" sportsName="football" city="padi" Gender="Male">
      <button> Click me </button>
